d3461e09578ad6a91a26cdcdae4e0e4c69823549
Toniii03 <tonicaceres2003@gmail.com>
2025-04-02 09:53:32 +0200

    base del proyecto creado

---
+++ factur/src/main/java/org/facktur/factur/FacturApplication.java
@@ -0,0 +1,13 @@
+package org.facktur.factur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturApplication.class, args);
+	}
+
+}
---
+++ factur/src/test/java/org/facktur/factur/FacturApplicationTests.java
@@ -0,0 +1,13 @@
+package org.facktur.factur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
