3b9d474a2d5f31e4e477bd3beeb6914626928af8
Toniii03 <tonicaceres2003@gmail.com>
2025-06-08 18:06:11 +0200

    Cambios para el despliegue

--- factur/src/main/java/org/facktur/factur/config/SecurityConfig.java
+++ factur/src/main/java/org/facktur/factur/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://facktur.netlify.app"));
+        configuration.setAllowedOrigins(List.of("https://factuk-front.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
